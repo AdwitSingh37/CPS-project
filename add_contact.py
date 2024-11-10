7fd74980851387d27c5612b499f96b60646d04a2
Dishitha0607 <dishi0607@gmail.com>
2024-11-10 13:30:26 +0530

    Add files via upload

---
+++ add_contact.py
@@ -0,0 +1,18 @@
+#Importing
+import csv
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+
+# Function to add a new contact 
+def add_contacts():
+    name=simpledialog.askstring("Input","Enter name:")
+    phone=simpledialog.askstring("Input","Enter phone number:")
+    address=simpledialog.askstring("Input","Enter address:")
+
+    if name and phone and address:
+        with open ("contacts.csv",'a',newline='') as file:
+            writer=csv.writer(file)
+            writer.writerow([name,phone,address])
+        messagebox.showinfo("Contact added sucessfully!")
+    else:
+        messagebox.showwarning("Input error", "Please fill in all the feilds")
