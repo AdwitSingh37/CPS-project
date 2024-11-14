592a35f6793ee291e08042d3dd60d377d8ce1c5e
mahima1006 <140642352+mahima1006@users.noreply.github.com>
2024-11-14 21:00:16 +0530

    Update display_all.py

--- display_all.py
+++ display_all.py
@@ -5,7 +5,6 @@ def display_all_contacts():
         reader = csv.reader(file) #reader is the variable which now holds all the data of the csv file
         next(reader)  
         contacts = [f"Name: {row[0]}, Phone: {row[1]}, Address: {row[2]}" for row in reader] #list comprehension is used here. Formats the string and stores the data into a list
-        print(contacts)
     if contacts: #checks if atleast one contact is entered by the user
         for contact in contacts: #traverses through each contact row in the file and later print it
             print(contact)
