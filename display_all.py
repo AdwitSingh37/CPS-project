5fd7ffb3f5474f9e9b3471d1ec65946cf086184a
mahima1006 <140642352+mahima1006@users.noreply.github.com>
2024-11-14 14:38:38 +0530

    Update display_all.py

--- display_all.py
+++ display_all.py
@@ -2,12 +2,12 @@ from tkinter import messagebox
 import csv
 def display_all_contacts():
     with open('contacts.csv', mode='r') as file:
-        reader = csv.reader(file)
-        next(reader)  
-        contacts = [f"Name: {row[0]}, Phone: {row[1]}, Address: {row[2]}" for row in reader]
+        reader = csv.reader(file) #reader is the variable which now holds all the data of the csv file
+        next(reader)  #this skips the first row which represents the title for each column
+        contacts = [f"Name: {row[0]}, Phone: {row[1]}, Address: {row[2]}" for row in reader] #list comprehension is used here. "row" takes one row of the csv file at a time
         
-    if contacts:
-        for contact in contacts:
+    if contacts: #checks if atleast one contact is entered by the user
+        for contact in contacts: #traverses through each contact row in the file and later print it
             print(contact)
     else:
         messagebox.showinfo("All Contacts", "No contacts available.")
