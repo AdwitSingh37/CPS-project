fa4db84f83fef8e181522ccd3736c1f42f92e26b
mahima1006 <140642352+mahima1006@users.noreply.github.com>
2024-11-14 20:55:38 +0530

    Update fun_statistics.py

--- fun_statistics.py
+++ fun_statistics.py
@@ -12,15 +12,14 @@ def fun_statistics():
             for i in row[1]:
                 if i.isdigit():  
                     digit_counts[i] += 1
-    
-    # Prepare data for the bar chart
-    digits = list(digit_counts.keys())
+
+    digits = list(digit_counts.keys()) #data for the bar graph
     counts = list(digit_counts.values())
     
     # Create a bar chart
-    plt.figure(figsize=(8, 6))
-    plt.bar(digits, counts, color='indigo')
+    plt.figure(figsize=(8, 6)) #size of the bar chart window(width,height)
+    plt.bar(digits, counts, color='indigo')#x axis, y axis and colour
     plt.xlabel("Digits")
     plt.ylabel("Frequency")
     plt.title("Frequency of Each Digit in Phone Numbers")
-    plt.show()
+    plt.show() #shows the final chart on the screen
