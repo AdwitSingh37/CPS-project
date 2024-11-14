0d7c5de2cb908a7b69b08b280309b146aee0d448
Dishitha0607 <dishi0607@gmail.com>
2024-11-14 16:10:40 +0530

    Update add_contact.py

--- add_contact.py
+++ add_contact.py
@@ -6,9 +6,9 @@ from tkinter import simpledialog, messagebox
 
 # Function to add a new contact
 def add_contact():
-	name = simpledialog.askstring("Input", "Enter name:")
-	phone = simpledialog.askstring("Input", "Enter phone:")
-	if len(phone)!=10:
+	name = simpledialog.askstring("Input", "Enter name:") 
+	phone = simpledialog.askstring("Input", "Enter phone:") 
+	if len(phone)!=10:  #if 10 numbers are not available ask user to type again
 		messagebox.showwarning("Input Error", "Please enter valid phone number.")
 		phone = simpledialog.askstring("Input", "Enter phone:")
 	address=simpledialog.askstring("Input", "Enter address:")
@@ -19,10 +19,10 @@ def add_contact():
 			writer = csv.writer(file)
 			writer.writerow([name, phone, address])
 		messagebox.showinfo("Success", "Contact added successfully!")
-	elif( name and phone and not address):
+	elif( name and phone and not address): #if the user doesn't type address 
 		with open ("contacts.csv",'a',newline='') as file:
 			writer=csv.writer(file)
 			writer.writerow([name,phone,"Nil"])
 		messagebox.showinfo("Contact add","Contact added sucessfully without address!")
-	else:
+	else:  #if the user doesn't fill feilds other than address
 		messagebox.showwarning("Input Error", "Please fill in all fields.")
